e65b35f7e5a31b258d899a3f4a89b05520490831
Danil Dubrovsky <forwot163@gmail.com>
2016-08-14 18:38:05 +0400

    Tasks solved

--- src/test/java/io/pivotal/literx/Part09BlockingToReactive.java
+++ src/test/java/io/pivotal/literx/Part09BlockingToReactive.java
@@ -48,7 +48,9 @@ public class Part09BlockingToReactive {
 
 	// TODO Create a Flux for reading all users from the blocking repository, and run it with an elastic scheduler
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
-		return null;
+		return Flux
+				.defer(() -> Flux.fromIterable(repository.findAll())
+				.subscribeOn(Schedulers.elastic()));
 	}
 
 //========================================================================================
@@ -73,7 +75,10 @@ public class Part09BlockingToReactive {
 
 	// TODO Insert users contained in the Flux parameter in the blocking repository using a parallel scheduler
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		return flux
+				.publishOn(Schedulers.parallel())
+				.doOnNext(repository::save)
+				.then();
 	}
 
 //========================================================================================
@@ -94,7 +99,7 @@ public class Part09BlockingToReactive {
 
 	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams does not accept null values)
 	Mono<User> nullAwareUserToMono(User user) {
-		return null;
+		return Mono.justOrEmpty(user);
 	}
 
 }
